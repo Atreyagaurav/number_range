5f964688e02d9c2b7e8bfadff70f4cf8e3338955
Gaurav Atreya <allmanpride@gmail.com>
2023-05-31 12:51:15 -0400

    Add macro rules numrng and numvec

--- src/lib.rs
+++ src/lib.rs
@@ -825,3 +825,54 @@ mod tests {
         assert_eq!(rng.parse_str("").unwrap().collect::<Vec<f64>>(), vec![]);
     }
 }
+
+/// Macro rule for generating number range. The [`NumberRange<T>`] is
+/// made with default options, then parsed.
+///
+/// ```rust
+/// # use std::error::Error;
+/// # use number_range::numrng;
+/// #
+/// # fn main() -> Result<(), Box<dyn Error>> {
+/// let mut rng = numrng!("1,3:2:6,-4:-2");
+/// assert_eq!(format!("{}", rng), "1,3:2:6,-4:-2");
+/// assert_eq!(rng.collect::<Vec<i64>>(), vec![1, 3, 5, -4, -3, -2]);
+/// #     Ok(())
+/// # }
+/// ```
+#[macro_export]
+macro_rules! numrng {
+    () => {
+        $crate::NumberRange::default()
+    };
+    ($l:expr) => {
+        $crate::NumberRange::default().parse_str($l).unwrap()
+    };
+}
+
+/// Macro rule for generating number range into vec. The
+/// [`NumberRange<T>`] is made with default options, then parsed and
+/// collected into a Vec<T>.
+///
+/// ```rust
+/// # use std::error::Error;
+/// # use number_range::numvec;
+/// #
+/// # fn main() -> Result<(), Box<dyn Error>> {
+/// let mut rng: Vec<i64> = numvec!("1,3:2:6,-4:-2");
+/// assert_eq!(rng, vec![1, 3, 5, -4, -3, -2]);
+/// #     Ok(())
+/// # }
+/// ```
+#[macro_export]
+macro_rules! numvec {
+    () => {
+        vec![]
+    };
+    ($l:expr) => {
+        $crate::NumberRange::default()
+            .parse_str($l)
+            .unwrap()
+            .collect()
+    };
+}
